297729f8480d1851f991353cba8ee6cea5779d82
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2024-07-25 21:13:50 -0300

    Buscando uma categoria

--- gRPC/internal/database/category.go
+++ gRPC/internal/database/category.go
@@ -59,3 +59,14 @@ func (c *Category) FindByCourseID(courseID string) (Category, error) {
 
 	return Category{ID: id, Name: name, Description: description}, nil
 }
+
+func (c *Category) FindByID(categoryID string) (Category, error) {
+	var id, name, description string
+	err := c.db.QueryRow("SELECT c.id, c.name, c.description FROM category c WHERE c.id = $1", categoryID).Scan(&id, &name, &description)
+
+	if err != nil {
+		return Category{}, err
+	}
+
+	return Category{ID: id, Name: name, Description: description}, nil
+}
