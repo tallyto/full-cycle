b367762b0dee8a7921fd196c8ed436e392f51a32
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2024-07-28 14:28:00 -0300

    Registrando serviço

--- service-discovey-and-consul/commands.sh
+++ service-discovey-and-consul/commands.sh
@@ -23,20 +23,4 @@ mkdir /etc/consul.d
 
 mkdir /var/lib/consul
 
-consul agent -server -bootstrap-expect=3 -node=consulserver01 -bind=127.20.0.4 -data-dir=/var/lib/consul -config-dir=/etc/consul.d
-
-consul agent -server -bootstrap-expect=3 -node=consulserver02 -bind=127.20.0.3 -data-dir=/var/lib/consul -config-dir=/etc/consul.d
-
-
-consul agent -server -bootstrap-expect=3 -node=consulserver01 -data-dir=/consul/data -bind=0.0.0.0 -client=0.0.0.0 
-
-consul agent -server -bootstrap-expect=3 -node=consulserver02 -data-dir=/consul/data -bind=0.0.0.0 -client=0.0.0.0 
-
-consul agent -server -bootstrap-expect=3 -node=consulserver03 -data-dir=/consul/data -bind=0.0.0.0 -client=0.0.0.0 
-
-
-consul agent -data-dir=/var/lib/consul -config-dir=/etc/consul.d -bind=0.0.0.0 -client=0.0.0.0 
-
-
-
-consul join 127.20.0.3
+consul catalog nodes -service nginx
