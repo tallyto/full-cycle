9314a748b485b46997326215853a8be76d0833ec
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2024-07-28 10:17:23 -0300

    Iniciando um agende consul

---
+++ service-discovey-and-consul/commands.sh
@@ -0,0 +1,20 @@
+# Acessa o bash no container do consul
+docker exec -it consul01 sh
+
+# Inicia o consul em modo dev
+consul agent -dev
+
+# Motra os consul members
+consul members
+
+# Mostrar os nodes
+curl localhost:8500/v1/catalog/nodes
+
+# Instala o bind-tools
+apk -U add bind-tools
+
+# Busca todos os nodes e ips dos nodes registrado
+dig @localhost -p 8600
+
+# Busca todos os nodes e ips dos nodes do consul registrado
+dig @localhost -p 8600 consul01.node.consul
