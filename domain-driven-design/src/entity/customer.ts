ebc61bc5bb3a31ebcf2ec01dfc9ee6d5c83f6ba5
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2024-08-08 11:33:46 -0300

    Principio da autoavaliação

--- domain-driven-design/src/entity/customer.ts
+++ domain-driven-design/src/entity/customer.ts
@@ -2,20 +2,34 @@ class Custumer {
     _id: string;
     _name: string;
     _address: string;
-    _active = true;
+    _active = false;
 
     constructor(id: string, name: string, address: string) {
         this._id = id;
         this._name = name;
         this._address = address;
+        this.validate()
+    }
+
+    validate() {
+        if (this._name.length === 0) {
+            throw new Error("Name is required")
+        }
+        if (this._id.length) {
+            throw new Error("Id is required")
+        }
+    }
+
+    activate() {
+        if(this._address.length === 0) {
+            throw new Error("Address is mandatory to activate a customer");
+            
+        }
+        this._active = true;
     }
 
     changeName(name: string){
         this._name = name
-    }
-
-    activate() {
-        this._active = true;
     }
 
     deactivate() {
