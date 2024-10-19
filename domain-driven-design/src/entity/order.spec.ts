e30ec72bd415725c8427f0851d3c64e097eb15f9
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2024-10-19 15:21:46 -0300

    Relacionando Item com Product

--- domain-driven-design/src/entity/order.spec.ts
+++ domain-driven-design/src/entity/order.spec.ts
@@ -2,34 +2,46 @@
 import Order from "./order";
 import OrderItem from "./order_item";
 
-describe('Order unit tests', ()=> {
-    it("Should throw error when id is empty", ()=> {
-        expect(()=> {
+describe('Order unit tests', () => {
+    it("Should throw error when id is empty", () => {
+        expect(() => {
             const order = new Order("", "123", []);
         }).toThrow("Id is required");
     })
-    
-    it("Should throw error when customerId is empty", ()=> {
-        expect(()=> {
+
+    it("Should throw error when customerId is empty", () => {
+        expect(() => {
             const order = new Order("123", "", []);
         }).toThrow("CustomerId is required");
     })
 
-    it("Should throw error when items is empty", ()=> {
-        expect(()=> {
+    it("Should throw error when items is empty", () => {
+        expect(() => {
             const order = new Order("123", "123", []);
         }).toThrow("Items are required");
     })
 
-    it("Should calculate total", ()=> {
+    it("Should calculate total", () => {
+
+        const orderItem1 = new OrderItem("1", "Item 1", 100, "p1", 2);
+        const orderItem2 = new OrderItem("2", "Item 2", 200, "p2", 2);
+
         const order = new Order("123", "123", [
-            new OrderItem("1", "Item 1", 100),
-            new OrderItem("2", "Item 2", 200),
-            new OrderItem("3", "Item 3", 300)
+            orderItem1
         ]);
 
-        const total = order.total();
+        expect(order.total()).toBe(200);
 
-        expect(total).toBe(600);
+        const order2 = new Order("order2", "customer2", [orderItem1, orderItem2])
+
+        expect(order2.total()).toBe(600)
+    })
+
+    
+    it("Should throw error if the item qtd is less or equal than zero", () => {
+        expect(() => {
+            const orderItem1 = new OrderItem("1", "Item 1", 100, "p1", 0);
+            const order = new Order("123", "123", [orderItem1]);
+        }).toThrow("Quantity must be greater than 0")
     })
 })
--- domain-driven-design/src/entity/order.ts
+++ domain-driven-design/src/entity/order.ts
@@ -1,32 +1,35 @@
 import OrderItem from "./order_item";
 
 export default class Order {
-    private id: string;
-    private customerId: string;
-    private items: OrderItem[] = [];
+    private _id: string;
+    private _customerId: string;
+    private _items: OrderItem[] = [];
     private _total: number;
 
     constructor(id: string, customerId: string, items: OrderItem[]) {
-        this.id = id;
-        this.customerId = customerId;
-        this.items = items
+        this._id = id;
+        this._customerId = customerId;
+        this._items = items
         this._total = this.total();
         this.validate();
     }
 
     private validate(): void {
-        if (this.id.length === 0) {
+        if (this._id.length === 0) {
             throw new Error("Id is required");
         }
-        if (this.customerId.length === 0) {
+        if (this._customerId.length === 0) {
             throw new Error("CustomerId is required");
         }
-        if (this.items.length === 0) {
+        if (this._items.length === 0) {
             throw new Error("Items are required");
+        }
+        if (this._items.some(item => item.quantity <= 0)){
+            throw new Error("Quantity must be greater than 0")
         }
     }
 
     total(): number {
-        return this.items.reduce((acc, item) => acc + item.price, 0);
+        return this._items.reduce((acc, item) => acc + item.price, 0);
     }
 }
