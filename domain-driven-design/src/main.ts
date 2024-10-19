34ed671d27638a3e3416339f3f99a869a965acb5
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2024-10-19 15:40:27 -0300

    Jest vs SWC

--- domain-driven-design/src/main.ts
+++ domain-driven-design/src/main.ts
@@ -10,8 +10,8 @@ const address = new Address("Rua dos bobos", 0, "12345678", "São Paulo")
 customer.setAddress(address)
 customer.activate()
 
-const item1 = new OrderItem("1", "Item 1", 10)
-const item2 = new OrderItem("2", "Item 2", 20)
+const item1 = new OrderItem("1", "Item 1", 10, "product1", 1)
+const item2 = new OrderItem("2", "Item 2", 20, "product2", 2)
 
 const order = new Order("1", "123", [item1, item2])
 
