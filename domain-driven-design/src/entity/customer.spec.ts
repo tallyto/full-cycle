28f4f03100051635a7033d1ef282d2b7239709a9
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2024-10-19 21:32:29 -0300

    008 - Testando regra de reward points

--- domain-driven-design/src/entity/customer.spec.ts
+++ domain-driven-design/src/entity/customer.spec.ts
@@ -49,4 +49,16 @@ describe("Customer unit tests", ()=> {
 
         expect(()=> customer.activate()).toThrow("Address is mandatory to activate a customer")
     })
+
+    it("should add reward points", ()=> {
+        const customer = new Customer("1", "John")
+        
+        expect(customer.rewardPoints).toBe(0)
+
+        customer.addRewardPoints(10)
+        expect(customer.rewardPoints).toBe(10)
+
+        customer.addRewardPoints(10)
+        expect(customer.rewardPoints).toBe(20)
+    })
 })
