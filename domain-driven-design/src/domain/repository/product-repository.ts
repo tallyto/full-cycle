488125395b707018beb76443b360406b3235cea3
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2024-11-11 22:14:03 -0300

    003 - Entendendo camada de Infra

---
+++ domain-driven-design/src/domain/repository/product-repository.ts
@@ -0,0 +1,7 @@
+import RepositoryInterface from "../../repository-interface";
+import Product from "../entity/product";
+
+export default interface ProductRepositoryInterface extends RepositoryInterface<Product> {
+    findByName(name: string): Promise<Product>
+
+}
