7ed74b7043780da70ebc359989d0f14e4319801e
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2024-12-29 19:27:10 -0300

    003 - Definindo teste para registrar evento

---
+++ domain-driven-design/src/domain/event/shared/event-dispatcher.spec.ts
@@ -0,0 +1,10 @@
+describe("Domain events", ()=> {
+    it("should register an event handler", ()=> {
+        const eventDispatcher = new EventDispatcher()
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler()
+
+        eventDispatcher.register("ProductCreatedEvent", eventHandler)
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined()
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(1)
+    })
+})
