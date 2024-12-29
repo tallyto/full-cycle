b68092b45a266d6a71273bb649bb7969994f8307
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2024-12-29 19:43:47 -0300

    005 - Registrando nosso primeiro evento

--- domain-driven-design/src/domain/event/shared/event-dispatcher.ts
+++ domain-driven-design/src/domain/event/shared/event-dispatcher.ts
@@ -3,11 +3,20 @@ import EventHandlerInterface from "./event-handler.interface";
 import EventInterface from "./event.interface";
 
 export default class EventDispatcher implements EventDispatcherInterface {
+    private eventHandlers: { [eventName: string]: EventHandlerInterface[] } = {};
+
+    get getEventHandlers(): { [eventName: string]: EventHandlerInterface[] } {
+        return this.eventHandlers;
+    }
+
     notify(event: EventInterface): void {
         throw new Error("Method not implemented.");
     }
     register(eventName: string, eventHandler: EventHandlerInterface): void {
-        throw new Error("Method not implemented.");
+        if(!this.eventHandlers[eventName]) {
+            this.eventHandlers[eventName] = [];
+        }
+        this.eventHandlers[eventName].push(eventHandler);
     }
     unregister(eventName: string, eventHandler: EventHandlerInterface): void {
         throw new Error("Method not implemented.");
@@ -15,5 +24,6 @@ export default class EventDispatcher implements EventDispatcherInterface {
     unregisterAll(): void {
         throw new Error("Method not implemented.");
     }
+
     
 }
