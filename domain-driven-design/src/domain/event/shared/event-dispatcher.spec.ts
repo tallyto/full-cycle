fa4f9d58b502eb9ca3c02fdde2f9edaf418bdaf2
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2024-12-29 19:54:33 -0300

    006 - Desregistrando eventos

--- domain-driven-design/src/domain/event/shared/event-dispatcher.spec.ts
+++ domain-driven-design/src/domain/event/shared/event-dispatcher.spec.ts
@@ -7,8 +7,37 @@ describe("Domain events", ()=> {
         const eventHandler = new SendEmailWhenProductIsCreatedHandler()
 
         eventDispatcher.register("ProductCreatedEvent", eventHandler)
-        
+
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined()
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(1)
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler)
+    })
+
+    it("should unregister an event handler", () => {
+        const eventDispatcher = new EventDispatcher()
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler()
+
+        eventDispatcher.register("ProductCreatedEvent", eventHandler)
+
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined()
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler)
+
+        eventDispatcher.unregister("ProductCreatedEvent", eventHandler)
+
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(0)
+    })
+
+    it("should unregister all event handlers", () => {
+        const eventDispatcher = new EventDispatcher()
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler()
+
+        eventDispatcher.register("ProductCreatedEvent", eventHandler)
+
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined()
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler)
+
+        eventDispatcher.unregisterAll()
+
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeUndefined()
     })
 })
