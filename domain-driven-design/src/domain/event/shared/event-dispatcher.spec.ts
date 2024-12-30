cad11f869c6c03c93cb11e1eb24fb27996ca4478
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2024-12-29 21:22:50 -0300

    007 - Disparando eventos

--- domain-driven-design/src/domain/event/shared/event-dispatcher.spec.ts
+++ domain-driven-design/src/domain/event/shared/event-dispatcher.spec.ts
@@ -1,4 +1,5 @@
 import SendEmailWhenProductIsCreatedHandler from "../product/handler/send-email-when-product-is-created.handler"
+import ProductCreatedEvent from "../product/producted-created.event"
 import EventDispatcher from "./event-dispatcher"
 
 describe("Domain events", ()=> {
@@ -40,4 +41,28 @@ describe("Domain events", ()=> {
 
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeUndefined()
     })
+
+    it("should notify all event handlers", () => {
+        const eventDispatcher = new EventDispatcher()
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler()
+
+        eventDispatcher.register("ProductCreatedEvent", eventHandler)
+
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined()
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler)
+
+        const productCreatedEvent = new ProductCreatedEvent({
+            name: "Product 1",
+            description: "Product 1 description",
+            price: 10
+        })
+
+        // Quando o notify for executado o SendEmailWhenProductIsCreated.handle() deve ser executado
+        const spyEventHandler = jest.spyOn(eventHandler, "handle")
+        eventDispatcher.notify(productCreatedEvent)
+
+        expect(spyEventHandler).toHaveBeenCalled()
+
+    })
+
 })
