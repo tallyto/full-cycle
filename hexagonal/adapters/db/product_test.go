6124761e4f3cae0c4c7fa035a9538fb0e56f10f3
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2025-01-08 22:27:10 -0300

    012 - Preparando testes de banco de dados

---
+++ hexagonal/adapters/db/product_test.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+)
+
+var Db *sql.DB
+
+func setUp() {
+	Db, _ = sql.Open("sqlite3", ":memory:")
+	createTable(Db)
+	createProduct(Db)
+}
+
+func createTable(db *sql.DB) {
+	table := `CREATE TABLE products (
+        "id" string, 
+        "name" string, 
+        "price" float, 
+        "status" string );`
+
+	stmt, err := db.Prepare(table)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	stmt.Exec()
+
+}
+
+func createProduct(db *sql.DB) {
+	insert := `insert into products values("abc", "Product Test", 0, "disabled")`
+	stmt, err := db.Prepare(insert)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	stmt.Exec()
+}
--- hexagonal/go.mod
+++ hexagonal/go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
 	github.com/golang/mock v1.6.0
 	github.com/google/uuid v1.6.0
+	github.com/mattn/go-sqlite3 v1.14.24
 	github.com/stretchr/testify v1.10.0
 )
